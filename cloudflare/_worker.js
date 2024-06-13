1fe772db64673845056bf54e2641836626025e19
Alex <zuedev@gmail.com>
2024-06-13 03:16:05 +0100

    try adding an api handler

--- cloudflare/_worker.js
+++ cloudflare/_worker.js
@@ -1,12 +1,20 @@
+function apiHandler(pathname) {
+  switch (pathname) {
+    case "/api/hello":
+      return new Response("Hello, world!");
+    case "/api/goodbye":
+      return new Response("Goodbye, world!");
+    default:
+      return new Response("Not found", { status: 404 });
+  }
+}
+
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    if (url.pathname.startsWith("/api")) {
-      // TODO: Add your custom /api/* logic here.
-      return new Response("Ok");
-    }
-    // Otherwise, serve the static assets.
-    // Without this, the Worker will error and no assets will be served.
+
+    if (url.pathname.startsWith("/api/")) return apiHandler(url.pathname);
+
     return env.ASSETS.fetch(request);
   },
 };
