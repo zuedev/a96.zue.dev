bcbcc20e1ed34c83a19bf35fa1b6793d75fab1ae
Alex <zuedev@gmail.com>
2024-06-28 22:26:59 +0100

    better nac handling

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,9 +4,9 @@ import A96Line from "@/components/A96Line";
 
 export default function NavBar(props) {
   return (
-    <div>
-      <div className="flex flex-row justify-between items-center bg-black p-4">
-        <div className="flex flex-row items-center space-x-4">
+    <div className="bg-black">
+      <div className="flex flex-row justify-between items-center p-4 max-w-screen-xl mx-auto">
+        <div className="flex flex-row items-center space-x-4 ">
           <a href="/">
             <img src="/logo96.png" alt="Area 96 Digital" className="h-10" />
           </a>
